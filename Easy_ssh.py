d29771abfdfe1a60b25514c579fdfa914ed4d690
XxTheSunxX <128015084+XxTheSunxX@users.noreply.github.com>
2024-11-17 21:00:53 -0800

    Easy_ssh.py

--- Easy_ssh.py
+++ Easy_ssh.py
@@ -1,1 +1,68 @@
+"""Easy SSH Client; Written by, XxTheSunxX"""
+"""Developed 2023; Revised 11/2024"""
 
+import paramiko
+from colorama import Fore
+import socket
+import sys
+import time
+
+FLAG = True
+
+
+def main():
+    print(Fore.WHITE + "\n")
+    print("Easy SSH Client; Written by XxTheSunxX\n")
+
+    print("""
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌
+▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌
+▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌     ▐░▌          ▐░▌          ▐░▌       ▐░▌
+▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀       ▀▀▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌
+▐░▌          ▐░▌       ▐░▌          ▐░▌     ▐░▌                    ▐░▌          ▐░▌▐░▌       ▐░▌
+▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌ ▄▄▄▄▄▄▄▄▄█░▌     ▐░▌           ▄▄▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌
+▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌
+ ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀ 
+\n""")
+
+    COUNT = 0
+    while COUNT < 3:
+        HOST = input("What is the host address: ")
+        USERNAME = input("What is the username: ")
+        PASSWORD = input("What is the password: ")
+        try:
+            ssh_connect(HOST, USERNAME, PASSWORD)
+        except Exception as e1:
+            print(f"Cannot connect due to error: {e1}\n")
+            COUNT = COUNT + 1
+    print("Exiting script...")
+    sys.exit()
+
+
+def ssh_connect(host, username, password):
+    while  FLAG == True:
+
+        client = paramiko.client.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(host, username=username, password=password)
+        hostname = socket.gethostname()
+        
+        print(f"Connected to: {hostname}")
+        answer = input("Run command? (yes/no) ")
+        
+        while answer == "yes":
+            command = input(">>> ")
+            
+            if command == 'exit':
+                sys.exit()
+            else:
+                _stdin, _stdout,_stderr = client.exec_command(command)
+                print(_stdout.read().decode())
+                FLAG == True
+        else:
+            sys.exit()
+if __name__ == '__main__':
+    main()
